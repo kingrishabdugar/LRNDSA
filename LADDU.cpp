2a922477c9acbb36e4b0fae0103d92af016a50bf
Rishab Dugar <56007479+kingrishabdugar@users.noreply.github.com>
2020-09-30 19:35:16 +0530

    Create LADDU.cpp

---
+++ LADDU.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define l long
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define ld long double
+#define mp make_pair
+#define pii pair<int,int>
+#define mod 1000000007
+#define rep(i,n) for(int i=0;i<n;i++)
+#define all(v) v.begin(),v.end()
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll as;
+       string o;
+       cin>>as>>o;
+       ll total=0;
+       while(as--)
+       {
+           string ac;
+        //complete the program
+        
+       }
+       cout<<(total/(o=="INDIAN"?200:400))<<endl;
+    }
+}
