b54bbbf083c64bc09a0d2e1d9a6431976f9d24f6
Prasoon Jain <80613141+PrasoonJain2002@users.noreply.github.com>
2021-09-30 20:19:46 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    fast;
+    int i=0;
+    cin>>i;
+    while(i!=42){
+        cout<<i<<endl;
+        cin>>i;
+    }
+}
